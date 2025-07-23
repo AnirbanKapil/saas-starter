0cf53eabe396686f302a1b7c9f7af4afb839a8df
Anirban Kapil <anirbankapil@gmail.com>
2025-07-23 22:11:19 +0530

    Implement SignUp component with form handling and loading state

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSignUp } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button' 
 import { Input } from "@/components/ui/input"
+import Loader from '@/components/loader/Loader'
 import {
   Card,
   CardAction,
@@ -18,9 +19,34 @@ import {
 
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+  
+  const {isLoaded,signUp,setActive} = useSignUp()  
+  const [emailAddress,setEmailAddress] = useState("")
+  const [password,setPassword] = useState("")
+  const [pendingVerification,setPendingVerification] = useState(false)
+  const [error,setError] = useState("")
+  const [code,setCode] = useState("")
+  const [showPassword,setShowPassword] = useState("")
+
+  const router = useRouter()
+
+  if(!isLoaded){
+    return <Loader />
+  }
+
+  async function submit (e : React.FormEvent) {
+    e.preventDefault()
+    if(!isLoaded) return <Loader />
+
+    try {
+      await signUp.create({
+        emailAddress,
+        password
+      })
+    } catch (error) {
+      
+    }
+  }
 }
 
 export default SignUp
