a06d3bbda4914e1a5ec372a97f4721fc29d76210
Anirban Kapil <anirbankapil@gmail.com>
2025-07-27 22:08:02 +0530

    Refactor authentication middleware to use authMiddleware and handle user roles

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware , clerkClient} from '@clerk/nextjs';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const publicRoutes = ["/", "/api/webhook/register", "/sign-in", "/sign-up"]
+
+export default authMiddleware({
+  publicRoutes,
+  
+  async  afterAuth (auth, req : NextRequest) {
+    if(!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)){
+      return NextResponse.redirect(new URL("/sign-in",req.url))
+    }
+    if(auth.userId){
+      const user = await clerkClient.users.getUsers(auth.userId)
+      const role = user.publicMetadata.role as string | undefined
+    }
+  }
+  
+  
+});
+
+
 
 export const config = {
   matcher: [
