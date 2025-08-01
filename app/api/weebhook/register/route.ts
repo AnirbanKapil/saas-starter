6afb122ff918516eca8c6c68ff408cd7d84fd2c2
Anirban Kapil <anirbankapil@gmail.com>
2025-08-01 22:06:44 +0530

    Implement webhook verification and handle user.created event

--- app/api/weebhook/register/route.ts
+++ app/api/weebhook/register/route.ts
@@ -18,5 +18,37 @@ export async function POST(req:Request) {
     if(!svix_id || !svix_timestamp || !svix_signature){
         return new Response ("Error occured - No svix headers")
     }
+
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
     
+    const wh = new Webhook(WEEBHOOK_SECRET)
+
+    let evnt : WebhookEvent
+    
+    try {
+        evnt = wh.verify(body,{
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature,
+        }) as WebhookEvent
+    } catch (err) {
+        console.log("Error occured while verifying WeebHook . Error--",err)
+        return new Response("Error occured while erifying WeebHook",{status : 400}) 
+    }
+
+    const {id} = evnt.data
+    const eventType = evnt.type
+
+    if(eventType === "user.created"){
+        const {email_addresses , primary_email_address_id} = evnt.data
+
+        const primaryEmail = email_addresses.find(
+            (email) => email.id === primary_email_address_id
+        )
+
+        if(!primaryEmail){
+            return new Response("No primary email found",{status : 400})
+        }
+    }
 }
