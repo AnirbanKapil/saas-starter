ae20651c2da645bf0aa521cde0dc0cff2d1bb8e7
Anirban Kapil <anirbankapil@gmail.com>
2025-08-01 13:13:49 +0530

    bug fix in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,25 @@
 import { clerkMiddleware , clerkClient , createRouteMatcher} from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 
+interface SessionPublicMetadata {
+  role?: string | undefined;
+}
 
 const isPublicRoutes = createRouteMatcher(["/", "/api/webhook/register", "/sign-in", "/sign-up"]) 
 
+   
+
   export default clerkMiddleware ( async (auth, req : NextRequest) => {
 
-    const {userId} = await auth()
+    const {userId , sessionClaims} = await auth()
 
     if(!userId && !isPublicRoutes(req)){
       return NextResponse.redirect(new URL("/sign-in",req.url))
     }
     if(userId){
       try {
-         const user = await clerkClient.users.getUser(userId);
-        const role = user.publicMetadata.role as string | undefined
+        
+        const role = (sessionClaims.publicMetadata as SessionPublicMetadata).role
   
         if(role === "admin" && req.nextUrl.pathname === "/dashboard"){
           return NextResponse.redirect(new URL("/admin/dashboard",req.url))
