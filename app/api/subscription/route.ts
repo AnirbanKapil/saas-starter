1816b1ce76bcd1e6e2e9f3d3927c4724c9973047
Anirban Kapil <anirbankapil@gmail.com>
2025-08-04 21:38:24 +0530

    Implement GET endpoint for subscription status and update logic

--- app/api/subscription/route.ts
+++ app/api/subscription/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
+import { error } from "console";
 
 export async function POST() {
   const { userId } = await auth();
@@ -34,6 +35,56 @@ export async function POST() {
   } catch (error) {
     console.error("Error updating subscription:", error);
     return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+
+export async function GET () {
+  const {userId} = await auth()
+
+  if(!userId){
+    return NextResponse.json({error : "unauthorized"},{status : 400})
+  }
+
+  try {
+    const user = await prisma.user.findUnique(
+      {
+        where : {id : userId},
+        select : {
+          isSubscribed : true,
+          subscriptionEnds : true
+        }
+      }
+    )
+
+    if(!user){
+      return NextResponse.json({error : "User not found"} , {status : 401})
+    }
+
+    const now = new Date()
+
+    if(user.subscriptionEnds && user.subscriptionEnds < now){
+      await prisma.user.update(
+        {
+          where : {id : userId},
+          data : {
+            isSubscribed : false,
+            subscriptionEnds : null
+          }
+        }
+      )
+    }
+
+     return NextResponse.json({
+      isSubscribed: user.isSubscribed,
+      subscriptionEnds: user.subscriptionEnds,
+    });
+  } catch (error) {
+     console.error("Error fetching subscription status:", error);
+     return NextResponse.json(
       { error: "Internal server error" },
       { status: 500 }
     );
